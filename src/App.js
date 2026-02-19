c08b38b67f9e7faacd1400507ba67e2eb728d923
ShuangyiHu <hu.shua@northeastern.edu>
2026-02-18 17:40:55 -0800

    Add route to user places list and finish styles for place items

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import {
 } from "react-router-dom";
 import Users from "./users/pages/Users";
 import NewPlace from "./places/pages/NewPlace";
+import UserPlaces from "./places/pages/UserPlaces";
 import MainNavigation from "./shared/components/Navigation/MainNavigation";
 
 function App() {
@@ -16,6 +17,9 @@ function App() {
         <Switch>
           <Route path="/" exact>
             <Users />
+          </Route>
+          <Route path="/:userId/places" exact>
+            <UserPlaces />
           </Route>
           <Route path="/places/new" exact>
             <NewPlace />
---
+++ src/places/pages/UserPlaces.js
@@ -0,0 +1,38 @@
+import PlaceList from "../components/PlaceList";
+
+const DUMMY_PLACES = [
+  {
+    id: "p1",
+    title: "Space Needle",
+    description:
+      "Iconic, 605-ft-tall spire at the Seattle Center, with an observation deck & a rotating restaurant.",
+    image:
+      "https://insightpestnorthwest.com/wp-content/uploads/2021/04/andrea-leopardi-QfhbK2pY0Ao-unsplash-1-1024x683.jpg",
+    address: "400 Broad St, Seattle, WA 98109",
+    coordinates: {
+      lat: 47.6205063,
+      lng: -122.3518523,
+    },
+    creatorId: "u1",
+  },
+  {
+    id: "p2",
+    title: "Space Needle",
+    description:
+      "Iconic, 605-ft-tall spire at the Seattle Center, with an observation deck & a rotating restaurant.",
+    image:
+      "https://insightpestnorthwest.com/wp-content/uploads/2021/04/andrea-leopardi-QfhbK2pY0Ao-unsplash-1-1024x683.jpg",
+    address: "400 Broad St, Seattle, WA 98109",
+    coordinates: {
+      lat: 47.6205063,
+      lng: -122.3518523,
+    },
+    creatorId: "u2",
+  },
+];
+
+const UserPlaces = () => {
+  return <PlaceList items={DUMMY_PLACES} />;
+};
+
+export default UserPlaces;
