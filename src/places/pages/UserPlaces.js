2250f0e20d3a76f5345b107f1914973675b341d4
ShuangyiHu <hu.shua@northeastern.edu>
2026-02-18 17:48:09 -0800

    Filter places shown on each user's page with userId

--- src/places/pages/UserPlaces.js
+++ src/places/pages/UserPlaces.js
@@ -1,3 +1,5 @@
+import { useParams } from "react-router-dom";
+
 import PlaceList from "../components/PlaceList";
 
 const DUMMY_PLACES = [
@@ -32,7 +34,9 @@ const DUMMY_PLACES = [
 ];
 
 const UserPlaces = () => {
-  return <PlaceList items={DUMMY_PLACES} />;
+  const userId = useParams().userId;
+  const loadedPlaces = DUMMY_PLACES.filter((p) => p.creatorId === userId);
+  return <PlaceList items={loadedPlaces} />;
 };
 
 export default UserPlaces;
