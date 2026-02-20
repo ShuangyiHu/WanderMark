d206ee9b2064e841805d9e095e7453cf7ec156ee
ShuangyiHu <hu.shua@northeastern.edu>
2026-02-19 16:45:41 -0800

    Create custom useForm hook to be reused on NEW PLACEand UPDATE PLACE forms

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -1,63 +1,30 @@
-import { act, useCallback, useDeferredValue, useReducer } from "react";
 import Input from "../../shared/components/FormElements/Input";
 import Button from "../../shared/components/FormElements/Button";
 import {
   VALIDATOR_MINLENGTH,
   VALIDATOR_REQUIRE,
 } from "../../shared/utils/validators";
+import useForm from "../../shared/hooks/form-hook";
 
 import "./PlaceForm.css";
 
-const formReducer = (state, action) => {
-  switch (action.type) {
-    case "INPUT_CHANGE":
-      let formIsValid = true;
-      for (const inputId in state.inputs) {
-        if (inputId === action.inputId) {
-          formIsValid = formIsValid && action.isValid;
-        } else {
-          formIsValid = formIsValid && state.inputs[inputId].isValid;
-        }
-      }
-      return {
-        ...state,
-        inputs: {
-          ...state.inputs,
-          [action.inputId]: { value: action.value, isValid: action.isValid },
-        },
-        isValid: formIsValid,
-      };
-    default:
-      return state;
-  }
-};
-
 const NewPlace = () => {
-  const [formState, dispatch] = useReducer(formReducer, {
-    inputs: {
-      title: {
-        value: "",
-        isValid: false,
-      },
-      description: {
-        value: "",
-        isValid: false,
-      },
-      address: {
-        value: "",
-        isValid: false,
-      },
+  const inputs = {
+    title: {
+      value: "",
+      isValid: false,
     },
-    isValid: false,
-  });
-  const inputHandler = useCallback((id, value, isValid) => {
-    dispatch({
-      type: "INPUT_CHANGE",
-      inputId: id,
-      value: value,
-      isValid: isValid,
-    });
-  }, []);
+    description: {
+      value: "",
+      isValid: false,
+    },
+    address: {
+      value: "",
+      isValid: false,
+    },
+  };
+
+  const [formState, inputHandler] = useForm(inputs, false);
 
   const placeSubmitHandler = (event) => {
     event.preventDefault();
