27c03596ff5978849f19e2835a97633ee822a52e
ShuangyiHu <hu.shua@northeastern.edu>
2026-02-20 13:05:46 -0800

    Add places-router and error handling middleware

---
+++ backend/app.js
@@ -0,0 +1,22 @@
+import express from "express";
+import placesRoutes from "./routes/places-routes.js";
+
+const app = express();
+
+app.use(express.json());
+
+app.use("/api/places", placesRoutes);
+
+// error handling middleware
+app.use((error, req, res, next) => {
+  if (res.headersSent) {
+    return next(error);
+  }
+  res
+    .status(error.code || 500)
+    .json({ message: error.message || "An unknown error occurred." });
+});
+
+app.listen(5001, () => {
+  console.log("Server running on port 5001");
+});
---
+++ backend/routes/places-routes.js
@@ -0,0 +1,63 @@
+import express from "express";
+
+const router = express.Router();
+
+const DUMMY_PLACES = [
+  {
+    id: "p1",
+    title: "Space Needle",
+    description:
+      "Iconic, 605-ft-tall spire at the Seattle Center, with an observation deck & a rotating restaurant.",
+    image:
+      "https://insightpestnorthwest.com/wp-content/uploads/2021/04/andrea-leopardi-QfhbK2pY0Ao-unsplash-1-1024x683.jpg",
+    address: "400 Broad St, Seattle, WA 98109",
+    coordinates: {
+      lat: 47.6205063,
+      lng: -122.3518523,
+    },
+    creatorId: "u1",
+  },
+  {
+    id: "p2",
+    title: "Space Needle",
+    description:
+      "Iconic, 605-ft-tall spire at the Seattle Center, with an observation deck & a rotating restaurant.",
+    image:
+      "https://insightpestnorthwest.com/wp-content/uploads/2021/04/andrea-leopardi-QfhbK2pY0Ao-unsplash-1-1024x683.jpg",
+    address: "400 Broad St, Seattle, WA 98109",
+    coordinates: {
+      lat: 47.6205063,
+      lng: -122.3518523,
+    },
+    creatorId: "u2",
+  },
+];
+
+router.get("/", (req, res, next) => {
+  console.log("GET request in Places");
+  res.json({ message: "It works!" });
+});
+
+router.get("/:placeId", (req, res, next) => {
+  const placeId = req.params.placeId;
+  const place = DUMMY_PLACES.find((p) => p.id === placeId);
+  if (!place) {
+    const error = new Error("No place was found for the provided place id.");
+    error.code = 404;
+    throw error;
+  }
+  res.json({ place });
+});
+
+router.get("/user/:userId", (req, res, next) => {
+  const userId = req.params.userId;
+  const places = DUMMY_PLACES.filter((p) => p.creatorId === userId);
+  if (places.length === 0) {
+    const error = new Error("No place was found for the provided place id.");
+    error.code = 404;
+    return next(error);
+  }
+  res.json({ places });
+});
+
+export default router;
