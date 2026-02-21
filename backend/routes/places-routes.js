6dca1d8abc8cc8e877089408f3b7c5f86ed35f84
ShuangyiHu <hu.shua@northeastern.edu>
2026-02-20 16:28:34 -0800

    Add validators to user inputs

--- backend/routes/places-routes.js
+++ backend/routes/places-routes.js
@@ -1,4 +1,6 @@
 import express from "express";
+import { check } from "express-validator";
+
 import {
   getPlaceById,
   getPlacesByUserId,
@@ -18,9 +20,21 @@ router.get("/:placeId", getPlaceById);
 
 router.get("/user/:userId", getPlacesByUserId);
 
-router.post("/", createPlace);
+router.post(
+  "/",
+  [
+    check("title").not().isEmpty(),
+    check("description").isLength({ min: 5 }),
+    check("address").not().isEmpty(),
+  ],
+  createPlace,
+);
 
-router.patch("/:placeId", updatePlaceById);
+router.patch(
+  "/:placeId",
+  [check("title").not().isEmpty(), check("description").isLength({ min: 5 })],
+  updatePlaceById,
+);
 
 router.delete("/:placeId", deletePlaceById);
 
