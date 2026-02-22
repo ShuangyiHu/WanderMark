8706153231ecb92638ee77605db4bdeba76def56
ShuangyiHu <hu.shua@northeastern.edu>
2026-02-21 20:04:10 -0800

    Enable CURD operations on users

---
+++ backend/models/user.js
@@ -0,0 +1,14 @@
+import { Schema, model } from "mongoose";
+import uniqueValidator from "mongoose-unique-validator";
+
+const userSchema = new Schema({
+  username: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true, minlength: 6 },
+  image: { type: String, required: true }, //url
+  places: { type: String, required: true },
+});
+
+userSchema.plugin(uniqueValidator);
+
+export default model("User", userSchema);
