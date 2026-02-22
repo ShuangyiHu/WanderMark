133298a5218a54f42a0da8dac4db6a681be5de46
ShuangyiHu <hu.shua@northeastern.edu>
2026-02-22 10:44:11 -0800

    Finish fetch users

--- frontend/src/users/pages/Auth.js
+++ frontend/src/users/pages/Auth.js
@@ -31,11 +31,34 @@ const Auth = () => {
 
   const authSubmitHandler = async (event) => {
     event.preventDefault();
+    setIsLoading(true);
 
     if (isLoginMode) {
+      try {
+        const response = await fetch("http://localhost:5001/api/users/login", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            email: formState.inputs.email?.value,
+            password: formState.inputs.password?.value,
+          }),
+        });
+        const data = await response.json();
+        if (!response.ok) {
+          throw new Error(data.message);
+        }
+
+        setIsLoading(false);
+        login();
+      } catch (err) {
+        console.log(err);
+        setIsLoading(false);
+        setError(err.message || "Something went wrong. Please try again later");
+      }
     } else {
       try {
-        setIsLoading(true);
         const response = await fetch("http://localhost:5001/api/users/signup", {
           method: "POST",
           headers: {
@@ -53,7 +76,6 @@ const Auth = () => {
         }
 
         setIsLoading(false);
-        console.log(data);
         login();
       } catch (err) {
         console.log(err);
