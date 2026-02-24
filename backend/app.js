6f3845f4d6ddd43d49eaea2849aac6bcc998cdf3
ShuangyiHu <hu.shua@northeastern.edu>
2026-02-24 13:26:12 -0800

    fix multipart boundary issue for cloudinary upload

--- backend/app.js
+++ backend/app.js
@@ -15,12 +15,25 @@ const app = express();
 app.use(express.json());
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://wander-mark-epfuljny9-amandashuangyihu-9253s-projects.vercel.app/",
+  );
+
   res.setHeader(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept, Authorization",
   );
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
+
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PATCH, DELETE, OPTIONS",
+  );
+
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+
   next();
 });
 
